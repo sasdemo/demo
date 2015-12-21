70cb7c983aeff02bb0a5428888a1ebdc64d70d3c
Alex <saswork@hotmail.com>
2015-12-21 09:19:55 +0200

    first commit

---
+++ app/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+angular.module('myApp', [
+  'ngRoute',
+  'myApp.view1',
+  'myApp.view2',
+  'myApp.version'
+]).
+config(['$routeProvider', function($routeProvider) {
+
+  $routeProvider.when('/home', {
+    templateUrl: 'view1/view1.html'
+    //controller: 'PhoneListCtrl'
+  }).otherwise({redirectTo: '/home'});
+}]);
