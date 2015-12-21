14de424182aa2a4c761994b3d36314e5c5cc36c9
Alex <saswork@hotmail.com>
2015-12-21 10:19:57 +0200

    edit

--- app/app.js
+++ app/app.js
@@ -1,16 +1,33 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
+var myApp = angular.module('myApp', [
   'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
+  //'myApp.view1',
+  //'myApp.view2',
+  //'myApp.version'
 ]).
 config(['$routeProvider', function($routeProvider) {
 
-  $routeProvider.when('/home', {
-    templateUrl: 'view1/view1.html'
-    //controller: 'PhoneListCtrl'
-  }).otherwise({redirectTo: '/home'});
+  $routeProvider
+      .when('/projects', {
+        templateUrl: 'partials/projects.html',
+        controller: 'NavController'
+      })
+      .when('/about', {
+        templateUrl: 'partials/about.html',
+        controller: 'NavController'
+      })
+      .when('/contact', {
+        templateUrl: 'partials/contact.html',
+        controller: 'NavController'
+      })
+      .otherwise({redirectTo: '/projects'});
+}]);
+
+myApp.controller('NavController', ['$scope', '$location', function($scope, $location) {
+    $scope.navClass = function (page) {
+      var currentRoute = $location.path().substring(1) || 'projects';
+      return page === currentRoute ? 'active' : '';
+    };
 }]);
